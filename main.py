a5087d224a98319ab31a6dc2919fab9115b04a60
Herold <heroldaraujo7@gmail.com>
2025-12-05 17:34:53 -0300

    Preparando para deploy no Render

--- main.py
+++ main.py
@@ -38,10 +38,9 @@ usuario_data = {}
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data() .decode('utf-8')
+        json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-        threading.Thread(target=bot.process_new_updates, args=([update],)).start()
         return 'Deu Certo', 200
     else: 
         return 'Deu errado', 400
@@ -116,3 +115,8 @@ def processar_mensagem(message: types.Message):
 
         # Limpa dados
         usuario_data.pop(chat_id, None)
+
+# ===== Inicialização para Render =====
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
